08e5f08fe58ce113c750746b1e87fe9646c444c5
yamaimo <yarnaimo@gmail.com>
2020-07-25 14:14:03 +0900

    Update adapter.ts

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,1 +1,17 @@
-export const adapter = ({}: { selector: a }) => {}
+type Selectors<T> = <F>(ref: FireTypes.Query<T, F>) => unknown
+
+type Adapter<T> = {
+  selectors: Selectors<T>
+}
+
+export const adapter = <T>() => {
+  return <S extends Selectors<T>>({
+    selectors = () => ({})
+  }: {
+    selectors?: S
+  }) => {
+    return {
+      selectors,
+    }
+  }
+}
