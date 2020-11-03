3cf65224d7f68740125f9cce497949da902ed8af
yamaimo <yarnaimo@gmail.com>
2020-11-03 15:23:21 +0900

    return options if $documentSchema not transformed

--- src/firestore/factories/firestore-schema.ts
+++ src/firestore/factories/firestore-schema.ts
@@ -3,7 +3,7 @@ import { STypes } from '../STypes'
 export function $documentSchema<T, U = T>(options?: {
   decoder?: STypes.Decoder<T, U>
 }): STypes.DocumentSchema<T, U> {
-  return (null as unknown) as STypes.DocumentSchema<T, U>
+  return (options ?? {}) as STypes.DocumentSchema<T, U>
 }
 
 export const createFirestoreSchema = <S extends STypes.RootOptions.All>(
