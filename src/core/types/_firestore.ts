859cf620d4bf6da9150a946174118c0e8b5d8a84
yamaimo <yarnaimo@gmail.com>
2020-11-20 16:16:30 +0900

    close #14 upgrade prettier to 2.2

--- src/core/types/_firestore.ts
+++ src/core/types/_firestore.ts
@@ -1,4 +1,3 @@
-
 // type PathChars =
 //   | 'A'
 //   | 'B'
@@ -85,19 +84,21 @@
 //   | `{${C[typeof $docLabel]}}`
 //   | string
 
-export type ParseCollectionPath<P extends string> =
-  P extends `${infer C}/${infer DT}`
-    ? DT extends `${infer D}/${infer T}`
-      ? [C, ...ParseCollectionPath<T>]
-      : never
-    : [P]
-
-export type ParseDocumentPath<P extends string> =
-  P extends `${infer C}/${infer DT}`
-    ? DT extends `${infer D}/${infer T}`
-      ? [C, ...ParseDocumentPath<T>]
-      : [C]
+export type ParseCollectionPath<
+  P extends string
+> = P extends `${infer C}/${infer DT}`
+  ? DT extends `${infer D}/${infer T}`
+    ? [C, ...ParseCollectionPath<T>]
     : never
+  : [P]
+
+export type ParseDocumentPath<
+  P extends string
+> = P extends `${infer C}/${infer DT}`
+  ? DT extends `${infer D}/${infer T}`
+    ? [C, ...ParseDocumentPath<T>]
+    : [C]
+  : never
 
 // export type SPath<
 //   C extends STypes.RootOptions.All | STypes.CollectionOptions.All,
--- src/core/types/_functions.ts
+++ src/core/types/_functions.ts
@@ -3,10 +3,9 @@ import { $topicName } from '../constants'
 import { FunTypes } from '../types'
 import { Prev } from './_object'
 
-export type ParseFunctionPath<P extends string> =
-  P extends `${infer P1}-${infer P2}`
-    ? [P1, ...ParseFunctionPath<P2>]
-    : [P]
+export type ParseFunctionPath<
+  P extends string
+> = P extends `${infer P1}-${infer P2}` ? [P1, ...ParseFunctionPath<P2>] : [P]
 
 export type ExtractFunctionPaths<
   S extends FunTypes.NestedFunctions,
@@ -32,9 +31,9 @@ export type GetTopicMeta<
         ? N extends TN
           ? S[K]
           : never
-        // : S[K] extends FunTypes.NestedFunctions
-        // ? GetTopicMeta<S[K], TN, Prev[D]>
-        : never
+        : // : S[K] extends FunTypes.NestedFunctions
+          // ? GetTopicMeta<S[K], TN, Prev[D]>
+          never
     }[keyof S & string]
 
 export type ExtractTopicNames<
@@ -45,7 +44,7 @@ export type ExtractTopicNames<
   : {
       [K in keyof S & string]: S[K] extends FunTypes.Topic.Meta<any, any>
         ? S[K][typeof $topicName]
-        // : S[K] extends FunTypes.NestedFunctions
-        // ? ExtractTopicNames<S[K], Prev[D]>
-        : never
+        : // : S[K] extends FunTypes.NestedFunctions
+          // ? ExtractTopicNames<S[K], Prev[D]>
+          never
     }[keyof S & string]
