807440f686e2c926ba2099d7ab61da07bfe0f3d1
yamaimo <yarnaimo@gmail.com>
2021-06-26 21:50:31 +0900

    pass TypedSnap to transformer

--- src/hooks/useSnapData.ts
+++ src/hooks/useSnapData.ts
@@ -1,28 +1,66 @@
 import { useMemo } from 'react'
+import {
+  STypes,
+  TypedDocumentSnap,
+  TypedQueryDocumentSnap,
+  TypedQuerySnap,
+} from '../core'
 import { _web } from '../lib/firestore-types'
 
-export const useQuerySnapData = <U, V = U>(
-  querySnap: _web.QuerySnapshot<U> | undefined,
-  transformer?: (data: U, snap: _web.QueryDocumentSnapshot<U>) => V,
+export type QueryDocumentSnapTransformer<
+  S extends STypes.RootOptions.All,
+  L extends string,
+  U,
+  V,
+> = (data: U, snap: TypedQueryDocumentSnap<S, _web.Firestore, L, U>) => V
+
+export type DocumentSnapTransformer<
+  S extends STypes.RootOptions.All,
+  L extends string,
+  U,
+  V,
+> = (data: U, snap: TypedDocumentSnap<S, _web.Firestore, L, U>) => V
+
+export const useQuerySnapData = <
+  S extends STypes.RootOptions.All,
+  L extends string,
+  U,
+  V = U,
+>(
+  querySnap: TypedQuerySnap<S, _web.Firestore, L, U> | undefined,
+  transformer?: QueryDocumentSnapTransformer<S, L, U, V>,
+  snapshotOptions?: _web.SnapshotOptions,
 ): V[] | undefined =>
   useMemo(
     () =>
-      querySnap?.docs.map((snap) => {
-        const data = snap.data({ serverTimestamps: 'estimate' })
+      querySnap?.typedDocs.map((snap) => {
+        const data = snap.data({
+          serverTimestamps: 'estimate',
+          ...snapshotOptions,
+        })
         return transformer?.(data, snap) ?? (data as unknown as V)
       }),
     [querySnap],
   )
 
-export const useDocumentSnapData = <U, V = U>(
-  snap: _web.DocumentSnapshot<U> | undefined,
-  transformer?: (data: U, snap: _web.DocumentSnapshot<U>) => V,
+export const useDocumentSnapData = <
+  S extends STypes.RootOptions.All,
+  L extends string,
+  U,
+  V = U,
+>(
+  snap: TypedDocumentSnap<S, _web.Firestore, L, U> | undefined,
+  transformer?: DocumentSnapTransformer<S, L, U, V>,
+  snapshotOptions?: _web.SnapshotOptions,
 ): V | undefined =>
   useMemo(() => {
     if (!snap) {
       return undefined
     }
-    const data = snap.data({ serverTimestamps: 'estimate' })
+    const data = snap.data({
+      serverTimestamps: 'estimate',
+      ...snapshotOptions,
+    })
     if (!data) {
       return undefined
     }
